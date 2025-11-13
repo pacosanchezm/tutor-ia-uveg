27c54ddb18609cddd231b4be74db4691175ca10c
pacosanchezm <paco.sanchezm@gmail.com>
2025-11-13 08:03:51 -0600

    username2

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,6 +3,8 @@ import React, { useCallback, useEffect, useRef, useState } from "react";
 import { useSearchParams } from "next/navigation";
 import { v4 as uuidv4 } from "uuid";
 
+
+
 import Image from "next/image";
 
 // UI components
